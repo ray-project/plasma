159131c6d2f3340929ed42fda737f5479feb71ad
Richard Shin <ricshin@cs.berkeley.edu>
2016-09-08 12:28:38 -0700

    Fix syntax error in check-git-clang-format-output.sh

--- .travis/check-git-clang-format-output.sh
+++ .travis/check-git-clang-format-output.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
   # Not in a pull request, so compare against parent commit
-  base_commit = "HEAD^"
+  base_commit="HEAD^"
   echo "Running clang-format against parent commit $(git rev-parse $base_commit)"
 else
-  base_commit = "$TRAVIS_BRANCH"
+  base_commit="$TRAVIS_BRANCH"
   echo "Running clang-format against branch $base_commit, with hash $(git rev-parse $base_commit)"
 fi
 output="$(.travis/git-clang-format --binary clang-format-3.8 --commit $base_commit --diff)"
