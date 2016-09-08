de830e2ca4f925ef7e5382e8614bcce77ff67a0a
Richard Shin <ricshin@cs.berkeley.edu>
2016-09-08 01:55:04 -0700

    Try to fix .travis.yml yet again

---
+++ .travis/check-git-clang-format-output.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+output=`.travis/git-clang-format --binary clang-format-3.8 --commit $TRAVIS_BRANCH --diff`
+if [ "$output" == "no modified files to format" ] || [ "$output" == "clang-format did not modify any files" ] ; then
+  echo "clang-format passed."
+  exit 0
+else
+  echo "clang-format failed:"
+  echo $output
+  exit 1
+fi
