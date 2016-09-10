004ef49ac9d4efe3be36a75b4ecfd77ca8890b48
Philipp Moritz <pcmoritz@gmail.com>
2016-09-10 10:15:28 -0700

    add utils.c

---
+++ src/utils.c
@@ -0,0 +1,15 @@
+#include "plasma.h"
+
+char *sha1_to_hex(const unsigned char *sha1, char *buffer) {
+  static const char hex[] = "0123456789abcdef";
+  char *buf = buffer;
+
+  for (int i = 0; i < PLASMA_SHA1_SIZE; i++) {
+    unsigned int val = *sha1++;
+    *buf++ = hex[val >> 4];
+    *buf++ = hex[val & 0xf];
+  }
+  *buf = '\0';
+
+  return buffer;
+}
