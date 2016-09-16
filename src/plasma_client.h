e205802d7aee9346e2f3fb282b8c570920a7ea59
Philipp Moritz <pcmoritz@gmail.com>
2016-09-16 00:45:10 -0700

    add more docs

--- src/plasma_client.h
+++ src/plasma_client.h
@@ -8,6 +8,7 @@ plasma_store_conn *plasma_store_connect(const char *socket_name);
 /* Connect to a possibly remote plasma manager */
 int plasma_manager_connect(const char *addr, int port);
 
+/* Create a new plasma object. */
 void plasma_create(plasma_store_conn *conn,
                    plasma_id object_id,
                    int64_t size,
@@ -15,6 +16,7 @@ void plasma_create(plasma_store_conn *conn,
                    int64_t metadata_size,
                    uint8_t **data);
 
+/* Get a plasma object. */
 void plasma_get(plasma_store_conn *conn,
                 plasma_id object_id,
                 int64_t *size,
@@ -22,6 +24,10 @@ void plasma_get(plasma_store_conn *conn,
                 int64_t *metadata_size,
                 uint8_t **metadata);
 
+/* Seal a plasma object. */
 void plasma_seal(plasma_store_conn *conn, plasma_id object_id);
+
+/* Must be called when a plasma object will not be used any more by a client. */
+void plasma_unmap(plasma_store_conn *conn, plasma_id object_id);
 
 #endif
